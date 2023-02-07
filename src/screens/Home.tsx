e29f14e8a49ff02c9cbc8227e91004d99f853078
Guilherme Bafica <gb.engcomp@hotmail.com>
2023-02-06 21:25:35 -0300

    Create HabitDay component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 import { Header } from '../components/Header';
+import { HabitDay, daySize } from '../components/HabitDay';
 
 export function Home() {
+  const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
 
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, weekDayIndex) => (
+          <Text
+            key={`${weekDayIndex} - ${weekDay}`}
+            className="text-zinc-400 text-xl font-bold text-center mx-1"
+            style={{ width: daySize }}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+
+      <HabitDay />
     </View>
   );
 }
